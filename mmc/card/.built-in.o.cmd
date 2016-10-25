0b74562e0e87847a372047230f31481f7fe96718
johnny <466243346@qq.com>
2016-10-25 13:40:09 +0800

    init

---
+++ mmc/card/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/mmc/card/built-in.o :=  arm-fsl-linux-gnueabi-ld -EL    -r -o drivers/mmc/card/built-in.o drivers/mmc/card/mmc_block.o 
---
+++ mmc/host/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/mmc/host/built-in.o :=  arm-fsl-linux-gnueabi-ld -EL    -r -o drivers/mmc/host/built-in.o drivers/mmc/host/mxs-mmc.o 
